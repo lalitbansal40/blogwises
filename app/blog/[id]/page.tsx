d7e93cead7228e2f9f96624b5dc2c25c182fe0be
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2025-05-20 00:19:13 +0530

    Update page.tsx

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,33 +1,33 @@
-// app/blog/[id]/page.tsx
 import { getBlogById } from '@/lib/getBlogs';
 import { Card, CardContent, Typography } from '@mui/material';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
-type Props = {
+type PageProps = {
   params: { id: string };
 };
 
-export default async function BlogPage({ params }: Props) {
+export default async function BlogPage({ params }: PageProps) {
   const blogPost = await getBlogById(params.id);
 
   if (!blogPost) return notFound();
 
   return (
     <div className="min-h-screen pt-12 px-4 w-full">
-      <div className=" mx-auto">
-        <Card className="">
-        <div className="space-y-2 p-2 pt-2 pb-12">
-              <Typography
-                variant="h3"
-                className="!font-extrabold !text-gray-900 leading-tight"
-              >
-                {blogPost.title}
-              </Typography>
-              <span className="mt-2 inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-md font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">
-                {blogPost.category}
-              </span>
-            </div>
+      <div className="mx-auto">
+        <Card>
+          <div className="space-y-2 p-2 pt-2 pb-12">
+            <Typography
+              variant="h3"
+              className="!font-extrabold !text-gray-900 leading-tight"
+            >
+              {blogPost.title}
+            </Typography>
+            <span className="mt-2 inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-md font-medium text-green-700 ring-1 ring-green-600/20 ring-inset">
+              {blogPost.category}
+            </span>
+          </div>
+
           <div className="relative w-full h-80 md:h-[28rem]">
             <Image
               src={blogPost.imageurl}
@@ -39,7 +39,6 @@ export default async function BlogPage({ params }: Props) {
           </div>
 
           <CardContent className="p-8 space-y-6">
-
             <Typography
               variant="body1"
               className="!text-gray-700 text-lg leading-relaxed"
@@ -47,9 +46,8 @@ export default async function BlogPage({ params }: Props) {
               {blogPost.description}
             </Typography>
 
-            <hr className=" my-6" />
+            <hr className="my-6" />
 
-            {/* Render HTML content safely */}
             <div
               className="prose prose-indigo max-w-none"
               dangerouslySetInnerHTML={{ __html: blogPost.content }}
