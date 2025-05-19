1424fd2e6b32813e0022afc916d01ec70bc252e1
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2025-05-20 00:13:06 +0530

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,9 @@
-// app/providers.tsx
 'use client';
 
 import { HeroUIProvider } from '@heroui/react';
-// Import any other providers if needed
 import { ReactNode } from 'react';
-// import EmotionProvider from '@/components/emotionProvider';
 import EmotionProvider from '@/components/emotionProvider';
+
 type ProvidersProps = {
   children: ReactNode;
 };
@@ -13,8 +11,9 @@ type ProvidersProps = {
 export function Providers({ children }: ProvidersProps) {
   return (
     <EmotionProvider>
-    <HeroUIProvider>
-      {children}
-    </HeroUIProvider>
+      <HeroUIProvider>
+        {children}
+      </HeroUIProvider>
+    </EmotionProvider>
   );
 }
