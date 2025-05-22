e4d4b6e6028cece8236619a0d140f53a480cd162
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2025-05-23 00:26:06 +0530

    Update Head.tsx

--- app/Head.tsx
+++ app/Head.tsx
@@ -5,7 +5,7 @@ export default function Head() {
       <title>Blog Wises</title>
       <meta name="description" content="Discover thoughtful articles and insights on Blog Wises." />
       <meta name="google-site-verification" content="EieLob6DcEfRLtpYU4ibHRE3VsyRQSEzvtEHa3TT9Sg" />
-     
+     <meta name="robots" content="index, follow" />
     </>
   );
 }
