e45fb497cd20c0de34c2aa7e9f55bb6fc4688822
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2025-05-24 13:27:52 +0530

    Update BlogDetail.tsx

--- app/blog/BlogDetail.tsx
+++ app/blog/BlogDetail.tsx
@@ -38,7 +38,7 @@ export default async function BlogDetail({ id }: { id: string }) {
           />
         </div>
 
-        <CardContent className="p-8 space-y-6 ">
+        <CardContent className="p-8 space-y-6 w-lg m-2">
           <div
             className="prose prose-indigo max-w-none px-8 "
             dangerouslySetInnerHTML={{ __html: blogPost.content }}
