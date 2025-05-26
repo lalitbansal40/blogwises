3b55e4e531216ddd3abfb1e783a6bbc23ad41152
lalitbansal40 <165453425+lalitbansal40@users.noreply.github.com>
2025-05-26 22:31:27 +0530

    changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,23 +1,31 @@
 import { MetadataRoute } from 'next';
 import { getBlogs, ApiResponse } from "@/lib/getBlogs";
-export default async function sitemap(): MetadataRoute.Sitemap {
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const data: ApiResponse = await getBlogs(1);
     const postUrls = data.blogs.map((post) => ({
         url: `https://alltypesblogs.online/blog/${post.blogid}`
     }));
+
     return [
         {
             url: 'https://alltypesblogs.online',
             lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 1.0,
         },
         {
             url: 'https://alltypesblogs.online/about',
             lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
         },
         {
             url: 'https://alltypesblogs.online/contact',
             lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
         },
-        ...postUrls
+        ...postUrls,
     ];
 }
